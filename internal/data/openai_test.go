08028258111bc9abf912bbe9ed3b6ed0b66433f3
jas.jin <jas.jin@cloudsky.com>
2026-04-17 11:47:08 +0800

    feat(meta_hs):维护hs

--- internal/data/openai_test.go
+++ internal/data/openai_test.go
@@ -7,12 +7,6 @@ import (
 )
 
 func TestChat(t *testing.T) {
-	aiChat := NewOpenAIChat(&conf.Bootstrap{
-		Openai: &conf.OpenAI{
-			ApiKey:  "sk-DKPXsITvLkWVdtN_gTRPdTmv9ILx3VJ1FVy9AI8ur4R9P_3oM4INlil_Or8",
-			BaseUrl: "http://10.86.3.248:3000/v1",
-			Model:   "gpt-3.5-turbo",
-		},
-	})
+	aiChat := NewOpenAIChat(&conf.Bootstrap{})
 	aiChat.Chat(context.Background(), "", "")
 }
